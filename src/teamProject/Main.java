7237c9e6f9c47d16f74253293dedff68c40a5831
yupseunghan <sky690212@gmail.com>
2025-01-16 17:58:51 +0900

    틀잡기

--- src/teamProject/Main.java
+++ src/teamProject/Main.java
@@ -1,5 +1,6 @@
 package teamProject;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,10 +9,21 @@ public class Main {
 	static Scanner sc =new Scanner(System.in);
 	public static void main(String[] args) {
 		ArrayList<Program> program = new ArrayList<Program>();
+		Program p = new Program(null,"12:30","14:00","무한도전", "꿀잼");
 		char menu;
 		do {
-			System.out.print("주간 TV편성 프로그램\n1.프로그램 추가\n2.프로그램 수정\n3.프로그램 삭제\n4.프로그램 조회\n5.프로그램 종"
-			+ "료\n메뉴 선택: ");
+			System.out.print("주간 TV편성 프로그램\n"
+					+ "=====================================\n");
+			try {
+				
+				p.setDate("2020-12-12");
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(p);
+			System.out.println("=====================================");
+			System.out.print("1.프로그램 추가\n2.프로그램 수정\n3.프로그램 삭제\n4.프로그램 조회\n5.프로그램 종료\n메뉴선택: ");
 			menu=sc.next().charAt(0);
 			switch(menu) {
 			case'1':System.out.println("추가하는 메서드");
--- src/teamProject/Program.java
+++ src/teamProject/Program.java
@@ -1,10 +1,33 @@
 package teamProject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class Program {
-	private String programDay,programName,programExpain,programTime;
+	private Date programDate;
+	private String startTime,endTime;
+	private String programName,programExpain;
+	
+	@Override
+	public String toString() {
+		return "["+getDateStr()+" "+startTime+" ~ "+endTime+"] "+programName+" "+programExpain;
+	}
+	
+	public void setDate(String dateTime) throws ParseException{
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		this.programDate=format.parse(dateTime);
+	}
+	
+	private String getDateStr() {
+		if(programDate == null)
+			return null;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		return format.format(programDate);
+	}
 }
