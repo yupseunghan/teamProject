9d1343b5cfa35b5aed59546c390494fe104da563
jayc722 <jaewon8469@gmail.com>
2025-02-04 17:07:30 +0900

    0204 수정

--- src/teamProject/Main.java
+++ src/teamProject/Main.java
@@ -1,13 +1,10 @@
 package teamProject;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		ProgramProgram p = new ProgramProgram();
-		p.run();
-	}
-
-}
+    public static void main(String[] args) {
+       ProgramManager program = new ProgramManager();
+       program.run();
+    }
+ }
